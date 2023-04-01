5421392dd43ffde09b535a9b917584988e84a72d
Gonzalo Stoll <stollgonzalo@gmail.com>
2023-04-02 01:45:05 +0200

    Update about content

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -16,6 +16,7 @@ I work on a M2 Macbook Air (midnight blue), but my setup also includes:
 
 - Screen: [LG 27UL650-W 4K Ultra HD IPS 27”](https://www.computersalg.dk/i/5595771/lg-27ul650-w-69cm-27-zoll-led-ips-panel-4k-uhd-hdr-400-amd-freesync-h%C3%B6henverstellung)
 - Keyboard: [KBD67 V3 Aluminium Case Mirror](https://www.maxgaming.com/en/cases/kbd67-v3-aluminium-case-mirror), [Gazzew Bobagum Silent Linear 62g](https://www.eloquentclicks.com/product/gazzew-bobagum-silent-linear-switch-62g-clear-top/), [KBDfans Clear2048 Transparent Keycaps Set Cherry Profile](https://kbdfans.com/products/clear2048-keycaps-set)
+- Microphone: [Røde NT-USB Mini](https://rode.com/en/microphones/usb/nt-usb-mini)
 - Mouse: Magic mouse (black)`
 
 export default function AboutPage() {
