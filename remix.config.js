72cd16a34b74ff6a5f39b97cd902fc992f2ed80b
Gonzalo Stoll <stollgonzalo@gmail.com>
2023-04-08 16:41:37 +0200

    v2 update: Removing `serverBuildTarget`

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,11 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: 'vercel',
+  publicPath: '/build/',
+  serverBuildPath: 'api/index.js',
+  serverMainFields: ['main', 'module'],
+  serverModuleFormat: 'cjs',
+  serverPlatform: 'node',
+  serverMinify: false,
   // When running locally in development mode, we use the built in remix
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
