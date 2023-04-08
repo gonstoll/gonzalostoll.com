cbd43b6cec9863de81890dc8794da6053d2d84b3
Gonzalo Stoll <stollgonzalo@gmail.com>
2023-04-08 21:56:09 +0200

    Favicons and manifest

--- app/root.tsx
+++ app/root.tsx
@@ -64,6 +64,25 @@ export function links() {
       type: 'font/woff2',
       crossOrigin: 'anonymous',
     },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/favicons/apple-touch-icon-180-180.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicons/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicons/favicon-16x16.png',
+    },
+    {rel: 'manifest', href: '/site.webmanifest'},
+    {rel: 'icon', href: '/favicon.ico'},
   ]
 }
 
