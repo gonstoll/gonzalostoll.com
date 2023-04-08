d29fac0a4d4bb2212e8fa1e93dd09342f5f19635
Gonzalo Stoll <stollgonzalo@gmail.com>
2023-04-08 18:47:32 +0200

    Improving mobile menu on navigation

--- app/components/MobileNav.tsx
+++ app/components/MobileNav.tsx
@@ -1,5 +1,6 @@
-import {Link} from '@remix-run/react'
+import {Link, useNavigation} from '@remix-run/react'
 import * as React from 'react'
+import {classNames} from '~/utils/classNames'
 import NavLinks from './NavLinks'
 import ThemeSwitch from './ThemeSwitch'
 
@@ -25,12 +26,8 @@ function useStickyHeader() {
 
 export default function MobileNav() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false)
-  const mobileMenuClass = isMenuOpen
-    ? 'top-0 opacity-100'
-    : '-top-full opacity-70'
-  const menuBtnClass = isMenuOpen
-    ? 'before:rotate-45 after:-rotate-45'
-    : 'before:-translate-y-2 after:translate-y-2'
+  const navigation = useNavigation()
+  const isLoading = navigation.state !== 'idle'
 
   React.useEffect(() => {
     if (isMenuOpen) {
@@ -50,11 +47,23 @@ export default function MobileNav() {
         <button
           onClick={() => setIsMenuOpen(!isMenuOpen)}
           aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
-          className={`ml-4 flex h-8 w-8 items-center before:absolute before:h-1 before:w-8 before:rounded-xs before:bg-black before:transition-toggle before:duration-500 after:absolute after:h-1 after:w-8 after:rounded-xs after:bg-black  after:transition-toggle after:duration-500 dark:before:bg-white dark:after:bg-white ${menuBtnClass}`}
+          className={classNames(
+            'ml-4 flex h-8 w-8 items-center before:absolute before:h-1 before:w-8 before:rounded-xs before:bg-black before:transition-toggle before:duration-500 after:absolute after:h-1 after:w-8 after:rounded-xs after:bg-black  after:transition-toggle after:duration-500 dark:before:bg-white dark:after:bg-white',
+            {
+              'before:rotate-45 after:-rotate-45': isMenuOpen,
+              'before:-translate-y-2 after:translate-y-2': !isMenuOpen,
+            }
+          )}
         />
       </div>
       <div
-        className={`fixed bottom-0 left-0 right-0 z-10 flex max-h-full w-full items-end bg-white p-10 transition-top duration-500 dark:bg-black ${mobileMenuClass}`}
+        className={classNames(
+          'fixed bottom-0 left-0 right-0 z-10 flex max-h-full w-full items-end bg-white p-10 transition-top duration-500 dark:bg-black',
+          {
+            'top-0 opacity-100': isMenuOpen || isLoading,
+            '-top-full opacity-70': !isLoading && !isMenuOpen,
+          }
+        )}
       >
         <NavLinks
           type="mobile"
@@ -68,11 +77,13 @@ export default function MobileNav() {
 
 export function MobileStickyNav() {
   const {isVisible} = useStickyHeader()
-  const visibleClass = isVisible ? 'top-0' : '-top-20'
 
   return (
     <div
-      className={`fixed left-0 z-10 w-full bg-white px-5 py-4 transition-top duration-300 dark:bg-black sm:px-10 ${visibleClass}`}
+      className={classNames(
+        'fixed left-0 z-10 w-full bg-white px-5 py-4 transition-top duration-300 dark:bg-black sm:px-10',
+        {'top-0': isVisible, '-top-20': !isVisible}
+      )}
     >
       <MobileNav />
     </div>
