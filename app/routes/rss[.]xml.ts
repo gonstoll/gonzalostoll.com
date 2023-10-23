191e43082539b5c9b1ebe84af42eea6c15de9d6a
Gonzalo Stoll <gonzalostoll@gonzalo.local>
2023-10-23 12:33:47 +0200

    Adding more attributes to rss feed

--- app/routes/rss[.]xml.ts
+++ app/routes/rss[.]xml.ts
@@ -11,19 +11,23 @@ export async function loader({request}: LoaderArgs) {
   }
 
   const rss = `
-    <rss xmlns:blogChannel="${blogUrl}" version="2.0">
+    <rss xmlns:blogChannel="${blogUrl}" version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/">
       <channel>
         <title>Gonzalo Stoll Blog</title>
         <link>${blogUrl}</link>
         <description>Gonzalo Stoll's Blog</description>
         <language>en-us</language>
         <ttl>60</ttl>
+        <atom:link href="${getDomainUrl(
+          request,
+        )}/rss.xml" rel="self" type="application/rss+xml" />
         ${posts
           .map(post =>
             `
             <item>
               <title>${cdata(post.title)}</title>
               <description>${cdata(post.summary)}</description>
+              <dc:creator>Gonzalo Stoll</dc:creator>
               <pubDate>${
                 new Date(post.date).toISOString().split('T')[0] // YYYY-mm-dd
               }</pubDate>
